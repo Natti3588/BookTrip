fe73bfa2002ff6127da13f789c0f2ec8e7461ece
Taisei2 <tnbox3588git@gmail.com>
2026-04-30 14:41:11 +0900

    fix(frontend): Vite Proxyのパスを修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
   server: {
     proxy: {
-      "./api": "http://localhost:3000",
+      "/api": "http://localhost:3000",
     },
   },
 })
