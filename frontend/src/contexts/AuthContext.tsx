1494c3316e5b129dadfa244cb38c08eba1b4c3d6
Taisei2 <tnbox3588git@gmail.com>
2026-05-09 22:40:33 +0900

    docs(frontend): AuthContextにコメントを追加

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useMemo, useState } from "react"
+import { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react"
 import {
   getMe,
   type LoginInput,
@@ -24,29 +24,32 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
+    // クライアントのCookieが有効ならUserオブジェクトを返す
+    // getMeはHTTP STATUS401をnullで返す
     getMe()
       .then((data) => setCurrentUser(data))
       .finally(() => setIsLoading(false))
   }, [])
 
-  const signup = async (data: SignupInput) => {
+  const signup = useCallback(async (data: SignupInput) => {
     const user = await signupApi(data)
     setCurrentUser(user)
-  }
+  }, [])
 
-  const login = async (data: LoginInput) => {
+  const login = useCallback(async (data: LoginInput) => {
     const user = await loginApi(data)
     setCurrentUser(user)
-  }
+  }, [])
 
-  const logout = async () => {
+  const logout = useCallback(async () => {
     await logoutApi()
+    // サーバー側のログアウトに失敗しても、クライアント側の状態は破棄して強制ログアウトさせる
     setCurrentUser(null)
-  }
+  }, [])
 
   const value = useMemo(
     () => ({ currentUser, isLoading, signup, login, logout }),
-    [currentUser, isLoading],
+    [currentUser, isLoading, signup, login, logout],
   )
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
