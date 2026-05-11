7a63b27a88b1fd59b1efa0668ef308466f682fe4
Taisei2 <tnbox3588git@gmail.com>
2026-05-11 12:24:33 +0900

    feat(backend): デプロイ用に設定を追加

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,15 @@
 import { serve } from "@hono/node-server"
+import { serveStatic } from "@hono/node-server/serve-static"
 import { Hono } from "hono"
 import authRoute from "./routes/auth.js"
 import booksRoute from "./routes/books.js"
 
-const app = new Hono().route("/api/books", booksRoute).route("/api/auth", authRoute)
-
-serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`)
-  },
-)
+const app = new Hono()
+  .route("/api/books", booksRoute)
+  .route("/api/auth", authRoute)
+  .use("/*", serveStatic({ root: "./public" }))
+  .get("*", serveStatic({ path: "./public/index.html" }))
+// 本番環境では指定されたポートを使い、開発環境では3000を使う
+serve({ fetch: app.fetch, port: Number(process.env.PORT) || 3000 })
 
 export type AppType = typeof app
